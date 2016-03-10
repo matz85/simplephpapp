13784f7edca04eaa6670ebce397ee0de27b5b986
Mike <gcode187@gmail.com>
2016-03-09 20:26:45 -0500

    Create db&table.sql

---
+++ db&table.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE  IF NOT EXISTS 'project';
+
+
+USE project;
+
+CREATE TABLE 'newppl'(
+  first varchar(30),
+  last varchar(30),
+  email varchar(50),
+  dob date,
+  gender varchar(10),
+  pass varchar(20)
+);
