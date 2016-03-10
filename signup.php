c7c34315819e7085aeeb7b512d2f5636f5d58f25
Mike <gcode187@gmail.com>
2016-03-09 20:13:55 -0500

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,124 @@
+<html>
+	<head>
+		<title>Sign up Form</title>
+	</head>
+	<body>
+		<table cellspacing='13px' border='0px'>
+		<form action='signup_des.php' method='POST'>
+			<tr>
+			<td>First Name</td>
+			<td><input type='text' name='first_name' /></td>
+			<tr>
+			<td>Last Name</td>
+			<td><input type='text' name='last_name' /></td>
+			<tr>
+			<td>Email</td>
+			<td><input type='text' name='email' /></td>
+			<tr>
+			<td>Birthday</td>
+			<td><select name='year'>
+				<option>Year</option>
+				<option>1970</option>
+				<option>1971</option>
+				<option>1972</option>
+				<option>1973</option>
+				<option>1974</option>
+				<option>1975</option>
+				<option>1976</option>
+				<option>1977</option>
+				<option>1978</option>
+				<option>1979</option>
+				<option>1980</option>
+				<option>1981</option>
+				<option>1982</option>
+				<option>1983</option>
+				<option>1984</option>
+				<option>1985</option>
+				<option>1986</option>
+				<option>1987</option>
+				<option>1988</option>
+				<option>1989</option>
+				<option>1990</option>
+				<option>1991</option>
+				<option>1992</option>
+				<option>1993</option>
+				<option>1994</option>
+				<option>1995</option>
+				<option>1996</option>
+				<option>1997</option>
+				<option>1998</option>
+				<option>1999</option>
+				<option>2000</option>
+			</select>
+			<select name='month'>
+				<option>Month</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
+			</select>
+			<select name='day'>
+				<option>Day</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
+				<option>13</option>
+				<option>14</option>
+				<option>15</option>
+				<option>16</option>
+				<option>17</option>
+				<option>18</option>
+				<option>19</option>
+				<option>20</option>
+				<option>21</option>
+				<option>22</option>
+				<option>23</option>
+				<option>24</option>
+				<option>25</option>
+				<option>26</option>
+				<option>27</option>
+				<option>28</option>
+				<option>29</option>
+				<option>30</option>
+				<option>31</option>
+			</select></td>
+			</tr>
+			<tr>
+			<td>Gender</td>
+				<td><input type='radio' name='gender' value='male' />Male
+					<input type='radio' name='gender' value='female' />Female</td>
+			</tr>
+			<tr>
+			<td>Password</td>
+			<td><input type='password' name='pass' /></td>
+			</tr>
+			<tr>
+			<td>Retype Password</td>
+			<td><input type='password' name='repass' /></td>
+			</tr>
+			<tr>
+			<td><input type='submit' name='submit' value='submit' /></td>
+			</tr>
+		</table>
+	</body>
+</html>
+
+
+
